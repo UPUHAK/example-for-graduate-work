5a3ede9f370e90f50f549a8677e9f6d2bfebf7b6
Dmitryi Kuznetsov <d.w.kuznetsov@yandex.ru>
2025-05-17 04:13:59 +0300

    test

--- src/main/java/ru/skypro/homework/mapper/UserMapper.java
+++ src/main/java/ru/skypro/homework/mapper/UserMapper.java
@@ -5,7 +5,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 import ru.skypro.homework.dto.UserDTO;
 import ru.skypro.homework.model.User;
-
 @Mapper
 public interface UserMapper {
 
